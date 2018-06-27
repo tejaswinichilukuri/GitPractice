934f4a73a89a66c57454bd636df935b287069a51
Tejaswini Chilukuri <tchilukuri@gcs.com>
2018-06-27 17:53:39 +0530

    Signed-off-by: Tejaswini Chilukuri <tchilukuri@gcs.com>

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -1,6 +1,7 @@
 
 public class GitPractice {
-	public static void main(String args[]) {
-		System.out.println("java");
+	public static void main(String args[]) {	
+	System.out.println("1.8");
+	System.out.println("stash");
 	}
 }
